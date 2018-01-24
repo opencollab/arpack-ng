0617b0b6bf2635781ec84155f657b8cda6b2ebc1
caliarim <marco.caliari@univr.it>
2018-01-24 14:16:56 +0100

    Force the residual vector to be in the range of OP. (#79)
    Force the residual vector to be in the range of the operator OP for bmat .eq. 'I', too.

--- SRC/zgetv0.f
+++ SRC/zgetv0.f
@@ -239,24 +239,22 @@ c        | the generalized problem when B is possibly (singular).   |
 c        %----------------------------------------------------------%
 c
          call arscnd (t2)
-         if (bmat .eq. 'G') then
-            nopx = nopx + 1
-            ipntr(1) = 1
-            ipntr(2) = n + 1
-            call zcopy (n, resid, 1, workd, 1)
-            ido = -1
-            go to 9000
-         end if
+         nopx = nopx + 1
+         ipntr(1) = 1
+         ipntr(2) = n + 1
+         call zcopy (n, resid, 1, workd, 1)
+         ido = -1
+         go to 9000
       end if
 c 
 c     %----------------------------------------%
-c     | Back from computing B*(initial-vector) |
+c     | Back from computing OP*(initial-vector) |
 c     %----------------------------------------%
 c
       if (first) go to 20
 c
 c     %-----------------------------------------------%
-c     | Back from computing B*(orthogonalized-vector) |
+c     | Back from computing OP*(orthogonalized-vector) |
 c     %-----------------------------------------------%
 c
       if (orth)  go to 40
@@ -271,9 +269,9 @@ c     %------------------------------------------------------%
 c
       call arscnd (t2)
       first = .TRUE.
+      call zcopy (n, workd(n+1), 1, resid, 1)
       if (bmat .eq. 'G') then
          nbx = nbx + 1
-         call zcopy (n, workd(n+1), 1, resid, 1)
          ipntr(1) = n + 1
          ipntr(2) = 1
          ido = 2
