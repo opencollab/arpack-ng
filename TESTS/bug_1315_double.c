857f6f7a023491f7ab05335b06ac601a64ed036e
caliarim <marco.caliari@univr.it>
2018-01-25 08:53:12 +0100

    Update test to allow the initial vector in range of OP. (#79)
    When the initial vector is forced to be in the range of OP, ido is -1.

--- TESTS/bug_1315_double.c
+++ TESTS/bug_1315_double.c
@@ -73,7 +73,7 @@ int main() {
   dnaupd(&ido, bmat, &N, which, &nev, &tol, resid, &ncv, V, &ldv, iparam, ipntr,
 	 workd, workl, &lworkl, &info);
 
-  while(ido == 1) {
+  while(ido == -1 || ido == 1) {
 
     matVec(&(workd[ipntr[0]-1]), &(workd[ipntr[1]-1]));
 
