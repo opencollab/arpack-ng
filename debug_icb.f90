76b5c5017a79476a9ffa50d44ac5f4331b1ba5e2
Franck HOUSSEN <franck.houssen@inria.fr>
2018-04-07 22:45:18 +0200

    Initialize debug common block with "block data" (at Fortran side).

--- debug_icb.f90
+++ debug_icb.f90
@@ -37,16 +37,3 @@ subroutine debug_c(logfil_c, ndigit_c, mgetv0_c,                                
   mcgets = mcgets_c
   mceupd = mceupd_c
 end subroutine debug_c
-
-! Initialisation of the debug common block to "no debug".
-block data init
-common /debug/ logfil, ndigit, mgetv0,                                &
-               msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-               mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-               mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
-data logfil, ndigit, mgetv0,                                &
-     msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-     mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-     mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd &
-     / 6, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /
-end
