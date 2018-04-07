be538c87c20064d46646f8fe4ffaa619c45c7b4a
Franck HOUSSEN <franck.houssen@inria.fr>
2018-04-07 17:21:38 +0200

    Revert "Move init debug common block from debug_icb.f90 to debug.h : this way you"
    This reverts commit 22d1b3b0b56bf0651b402d18576848e1b51987fe.

--- debug.h
+++ debug.h
@@ -14,16 +14,3 @@
                msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
                mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
                mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
-
-! Initialisation of the debug common block to "no debug".
-block data init
-common /debug/ logfil, ndigit, mgetv0,                                &
-               msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-               mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-               mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
-data logfil, ndigit, mgetv0,                                &
-     msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-     mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-     mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd &
-     / 6, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /
-end
