f92349206ba9066d54fe06a3d6d0fc5105cb8944
Ruslan Kabatsayev <b7.10110111@gmail.com>
2018-06-07 12:33:41 +0300

    Fix correctness of C bindings
    Main problems found in the original version are:
    I. C vs iso_c_binding:
        1. Mismatch between float complex and float parameters in C prototypes and
        icba*.f90 files
        2. Mismatch between passing of some arguments by pointer vs by value
    II. iso_c_binding vs Fortran:
        1. Mismatch between real and complex parameters
        2. Wrong intent for 'workev'
    
    These problems lead to UB, which, in particular, resulted in test
    crashes on i686 targets. This patch fixes them.

--- SRC/icbazn.f90
+++ SRC/icbazn.f90
@@ -5,23 +5,23 @@ subroutine znaupd_c(ido, bmat, n, which, nev, tol, resid, ncv, v, ldv,&
                     bind(c, name="znaupd_c")
   use :: iso_c_binding
   implicit none
-  integer(kind=c_int),                              intent(inout) :: ido
-  character(kind=c_char),      dimension(1),        intent(in)    :: bmat
-  integer(kind=c_int),         value,               intent(in)    :: n
-  character(kind=c_char),      dimension(2),        intent(in)    :: which
-  integer(kind=c_int),         value,               intent(in)    :: nev
-  real(kind=c_double_complex), value,               intent(in)    :: tol
-  real(kind=c_double_complex), dimension(n),        intent(inout) :: resid
-  integer(kind=c_int),         value,               intent(in)    :: ncv
-  real(kind=c_double_complex), dimension(ldv, ncv), intent(out)   :: v
-  integer(kind=c_int),         value,               intent(in)    :: ldv
-  integer(kind=c_int),         dimension(11),       intent(inout) :: iparam
-  integer(kind=c_int),         dimension(11),       intent(out)   :: ipntr
-  real(kind=c_double_complex), dimension(3*n),      intent(out)   :: workd
-  real(kind=c_double_complex), dimension(lworkl),   intent(out)   :: workl
-  integer(kind=c_int),         value,               intent(in)    :: lworkl
-  real(kind=c_double_complex), dimension(ncv),      intent(out)   :: rwork
-  integer(kind=c_int),                              intent(inout) :: info
+  integer(kind=c_int),                                  intent(inout) :: ido
+  character(kind=c_char),           dimension(1),       intent(in)    :: bmat
+  integer(kind=c_int),              value,              intent(in)    :: n
+  character(kind=c_char),           dimension(2),       intent(in)    :: which
+  integer(kind=c_int),              value,              intent(in)    :: nev
+  real(kind=c_double),              value,              intent(in)    :: tol
+  complex(kind=c_double_complex),   dimension(n),       intent(inout) :: resid
+  integer(kind=c_int),              value,              intent(in)    :: ncv
+  complex(kind=c_double_complex),   dimension(ldv, ncv),intent(out)   :: v
+  integer(kind=c_int),              value,              intent(in)    :: ldv
+  integer(kind=c_int),              dimension(11),      intent(inout) :: iparam
+  integer(kind=c_int),              dimension(11),      intent(out)   :: ipntr
+  complex(kind=c_double_complex),   dimension(3*n),     intent(out)   :: workd
+  complex(kind=c_double_complex),   dimension(lworkl),  intent(out)   :: workl
+  integer(kind=c_int),              value,              intent(in)    :: lworkl
+  real(kind=c_double),              dimension(ncv),     intent(out)   :: rwork
+  integer(kind=c_int),                                  intent(inout) :: info
   call znaupd(ido, bmat, n, which, nev, tol, resid, ncv, v, ldv,&
               iparam, ipntr, workd, workl, lworkl, rwork, info)
 end subroutine znaupd_c
@@ -32,30 +32,30 @@ subroutine zneupd_c(rvec, howmny, select, d, z, ldz, sigma, workev,  &
                     bind(c, name="zneupd_c")
   use :: iso_c_binding
   implicit none
-  logical(kind=c_bool),        value,               intent(in)    :: rvec
-  character(kind=c_char),      dimension(1),        intent(in)    :: howmny
-  logical(kind=c_bool),        dimension(ncv),      intent(in)    :: select
-  real(kind=c_double_complex), dimension(nev),      intent(out)   :: d
-  real(kind=c_double_complex), dimension(n, nev),   intent(out)   :: z
-  integer(kind=c_int),         value,               intent(in)    :: ldz
-  real(kind=c_double_complex), value,               intent(in)    :: sigma
-  real(kind=c_double_complex), dimension(2*ncv),    intent(in)    :: workev
-  character(kind=c_char),      dimension(1),        intent(in)    :: bmat
-  integer(kind=c_int),         value,               intent(in)    :: n
-  character(kind=c_char),      dimension(2),        intent(in)    :: which
-  integer(kind=c_int),         value,               intent(in)    :: nev
-  real(kind=c_double_complex), value,               intent(in)    :: tol
-  real(kind=c_double_complex), dimension(n),        intent(inout) :: resid
-  integer(kind=c_int),         value,               intent(in)    :: ncv
-  real(kind=c_double_complex), dimension(ldv, ncv), intent(out)   :: v
-  integer(kind=c_int),         value,               intent(in)    :: ldv
-  integer(kind=c_int),         dimension(11),       intent(inout) :: iparam
-  integer(kind=c_int),         dimension(11),       intent(out)   :: ipntr
-  real(kind=c_double_complex), dimension(3*n),      intent(out)   :: workd
-  real(kind=c_double_complex), dimension(lworkl),   intent(out)   :: workl
-  integer(kind=c_int),         value,               intent(in)    :: lworkl
-  real(kind=c_double_complex), dimension(ncv),      intent(out)   :: rwork
-  integer(kind=c_int),                              intent(inout) :: info
+  logical(kind=c_bool),             value,              intent(in)    :: rvec
+  character(kind=c_char),           dimension(1),       intent(in)    :: howmny
+  logical(kind=c_bool),             dimension(ncv),     intent(in)    :: select
+  complex(kind=c_double_complex),   dimension(nev),     intent(out)   :: d
+  complex(kind=c_double_complex),   dimension(n, nev),  intent(out)   :: z
+  integer(kind=c_int),              value,              intent(in)    :: ldz
+  complex(kind=c_double_complex),   value,              intent(in)    :: sigma
+  complex(kind=c_double_complex),   dimension(2*ncv),   intent(out)   :: workev
+  character(kind=c_char),           dimension(1),       intent(in)    :: bmat
+  integer(kind=c_int),              value,              intent(in)    :: n
+  character(kind=c_char),           dimension(2),       intent(in)    :: which
+  integer(kind=c_int),              value,              intent(in)    :: nev
+  real(kind=c_double),              value,              intent(in)    :: tol
+  complex(kind=c_double_complex),   dimension(n),       intent(inout) :: resid
+  integer(kind=c_int),              value,              intent(in)    :: ncv
+  complex(kind=c_double_complex),   dimension(ldv, ncv),intent(out)   :: v
+  integer(kind=c_int),              value,              intent(in)    :: ldv
+  integer(kind=c_int),              dimension(11),      intent(inout) :: iparam
+  integer(kind=c_int),              dimension(11),      intent(out)   :: ipntr
+  complex(kind=c_double_complex),   dimension(3*n),     intent(out)   :: workd
+  complex(kind=c_double_complex),   dimension(lworkl),  intent(out)   :: workl
+  integer(kind=c_int),              value,              intent(in)    :: lworkl
+  real(kind=c_double),              dimension(ncv),     intent(out)   :: rwork
+  integer(kind=c_int),                               intent(inout) :: info
   call zneupd(rvec, howmny, select, d, z, ldz, sigma, workev,&
               bmat, n, which, nev, tol, resid, ncv, v, ldv,  &
               iparam, ipntr, workd, workl, lworkl, rwork, info)
