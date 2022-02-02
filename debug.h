881d6cd08f618cc4362461098f52795d071b78cb
Tom Payerle <payerle@umd.edu>
2022-02-02 11:30:14 -0500

    Change the continuation line format for stat.h, debug.h
    AOCC flang compiler is complaining about the continuation lines in these
    two files.  It does not complain about similar continuation lines in
    files (e.g. SRC/icbacn.F90), so I believe this is due to flang setting
    the Fortran dialect to something like F77 for most of the compilations,
    which cause problems with these *.h files.
    
    I cannot find a way to force flang Fortran dialect to accept the & at
    end of line continuation marker, so this patch changes the continuation
    lines in these two files (stat.h, debug.h) to use continuation marker in
    column 6.
    
    This should fix #300

--- debug.h
+++ debug.h
@@ -5,12 +5,12 @@
 !     %---------------------------------%
 !     | See debug.doc for documentation |
 !     %---------------------------------%
-      integer  logfil, ndigit, mgetv0,                                &
-               msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-               mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-               mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
-      common /debug/                                                  &
-               logfil, ndigit, mgetv0,                                &
-               msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,&
-               mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,&
-               mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
+      integer  logfil, ndigit, mgetv0,                                
+     &         msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,
+     &         mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,
+     &         mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
+      common /debug/                                                  
+     &         logfil, ndigit, mgetv0,                                
+     &         msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,
+     &         mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,
+     &         mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
--- stat.h
+++ stat.h
@@ -9,13 +9,13 @@
       save       t0, t1, t2, t3, t4, t5
 !
       integer    nopx, nbx, nrorth, nitref, nrstrt
-      real       tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv,&
-                 tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,&
-                 tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,&
-                 tmvopx, tmvbx, tgetv0, titref, trvec
-      common /timing/                                                   &
-                 nopx, nbx, nrorth, nitref, nrstrt,                     &
-                 tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv,&
-                 tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,&
-                 tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,&
-                 tmvopx, tmvbx, tgetv0, titref, trvec
+      real       tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv,
+     &           tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,
+     &           tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,
+     &           tmvopx, tmvbx, tgetv0, titref, trvec
+      common /timing/                                                   
+     &           nopx, nbx, nrorth, nitref, nrstrt,                     
+     &           tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv,
+     &           tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,
+     &           tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,
+     &           tmvopx, tmvbx, tgetv0, titref, trvec
