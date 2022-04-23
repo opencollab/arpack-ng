bca848b8e398fb9b2cb340c3c959aa40aaeb82a3
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-04-23 14:12:42 +0200

    .h with F77/F90 flavors.

--- ICB/debug_icb.F90
+++ ICB/debug_icb.F90
@@ -12,7 +12,7 @@ subroutine debug_c(logfil_c, ndigit_c, mgetv0_c,                                
   integer(kind=i_int), value, intent(in) :: msaupd_c, msaup2_c, msaitr_c, mseigt_c, msapps_c, msgets_c, mseupd_c
   integer(kind=i_int), value, intent(in) :: mnaupd_c, mnaup2_c, mnaitr_c, mneigh_c, mnapps_c, mngets_c, mneupd_c
   integer(kind=i_int), value, intent(in) :: mcaupd_c, mcaup2_c, mcaitr_c, mceigh_c, mcapps_c, mcgets_c, mceupd_c
-  include 'debug.h'
+  include 'debugF90.h'
   logfil = logfil_c
   ndigit = ndigit_c
   mgetv0 = mgetv0_c
