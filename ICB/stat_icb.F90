1f4c1e0a4454c3de2f881b69150fac50483879d2
Tom Payerle <payerle@umd.edu>
2022-04-23 12:42:25 -0400

    Change the continuation line format for stat.h, debug.h (#339)
    Change the continuation line format for stat.h, debug.h
    
    AOCC flang compiler is complaining about the continuation lines in these
    two files.  It does not complain about similar continuation lines in
    files (e.g. SRC/icbacn.F90), so I believe this is due to flang setting
    the Fortran dialect to something like F77 for most of the compilations,
    which cause problems with these *.h files.
    
    I cannot find a way to force flang Fortran dialect to accept the & at
    end of line continuation marker, so this patch changes the continuation
    lines in these two files (stat.h, debug.h) to use continuation marker in
    column 6.
    
    This should fix #300

--- ICB/stat_icb.F90
+++ ICB/stat_icb.F90
@@ -32,7 +32,7 @@ subroutine stat_c(  nopx_c,    nbx_c, nrorth_c, nitref_c, nrstrt_c,             
                                       tnaupd_c, tnaup2_c, tnaitr_c, tneigh_c, tngets_c, tnapps_c, tnconv_c,&
                                       tcaupd_c, tcaup2_c, tcaitr_c, tceigh_c, tcgets_c, tcapps_c, tcconv_c,&
                                       tmvopx_c,  tmvbx_c, tgetv0_c, titref_c,  trvec_c
-  include 'stat.h'
+  include 'statF90.h'
     nopx_c =   nopx
      nbx_c =    nbx
   nrorth_c = nrorth
