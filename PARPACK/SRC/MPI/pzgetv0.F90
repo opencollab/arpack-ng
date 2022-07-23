35c75960ce3261733a702fd400beb73aefcd73cf
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:49 +0200

    ./PARPACK/SRC/MPI/pzgetv0.F90: use statF90 header.

--- PARPACK/SRC/MPI/pzgetv0.F90
+++ PARPACK/SRC/MPI/pzgetv0.F90
@@ -146,7 +146,7 @@
 !     %----------------------------------------------------%
 !
 #include "debugF90.h"
-      include   'stat.h'
+#include "statF90.h"
 !
 !     %------------------%
 !     | Scalar Arguments |
