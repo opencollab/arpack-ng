5ece4dba5ad7b8fa0a7ebe4e1ba57c21975d76eb
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:50 +0200

    ./PARPACK/SRC/MPI/pzlarnv.F90: comment are !, not c.

--- PARPACK/SRC/MPI/pzlarnv.F90
+++ PARPACK/SRC/MPI/pzlarnv.F90
@@ -1,71 +1,71 @@
-c\BeginDoc
-c
-c\Name: pzlarnv
-c
-c Message Passing Layer: MPI
-c
-c\Description:
-c
-c  Parallel Version of ARPACK utility routine zlarnv
-c
-c  PZLARNV returns a vector of n (nloc) random Complex*16 numbers from a uniform or
-c  normal distribution. It is assumed that X is distributed across a 1-D array
-c  of processors ( nprocs < 1000 )
-c
-c\Arguments
-c  COMM    MPI Communicator for the processor grid
-c
-c  IDIST   (input) INTEGER
-c          Specifies the distribution of the random numbers:
-c          = 1:  uniform (0,1)
-c          = 2:  uniform (-1,1)
-c          = 3:  normal (0,1)
-c
-c  ISEED   (input/output) INTEGER array, dimension (4)
-c          On entry, the seed of the random number generator; the array
-c          elements must be between 0 and 4095, and ISEED(4) must be
-c          odd.
-c          On exit, the seed is updated.
-c
-c  N       (input) INTEGER
-c          The number of random numbers to be generated.
-c
-c  X       (output) Complex*16 array, dimension (N)
-c          The generated random numbers.
-c
-c\Author: Kristi Maschhoff
-c
-c\Details
-c
-c  Simple parallel version of LAPACK auxiliary routine zlarnv
-c  for X distributed across a 1-D array of processors.
-c  This routine calls the auxiliary routine CLARNV to generate random
-c  Complex*16 numbers from a uniform or normal distribution. Output is consistent
-c  with serial version.
-c
-c\SCCS Information:
-c FILE: larnv.F   SID: 1.3   DATE OF SID: 04/17/99
-c
-c-----------------------------------------------------------------------
-c
+!\BeginDoc
+!
+!\Name: pzlarnv
+!
+! Message Passing Layer: MPI
+!
+!\Description:
+!
+!  Parallel Version of ARPACK utility routine zlarnv
+!
+!  PZLARNV returns a vector of n (nloc) random Complex*16 numbers from a uniform or
+!  normal distribution. It is assumed that X is distributed across a 1-D array
+!  of processors ( nprocs < 1000 )
+!
+!\Arguments
+!  COMM    MPI Communicator for the processor grid
+!
+!  IDIST   (input) INTEGER
+!          Specifies the distribution of the random numbers:
+!          = 1:  uniform (0,1)
+!          = 2:  uniform (-1,1)
+!          = 3:  normal (0,1)
+!
+!  ISEED   (input/output) INTEGER array, dimension (4)
+!          On entry, the seed of the random number generator; the array
+!          elements must be between 0 and 4095, and ISEED(4) must be
+!          odd.
+!          On exit, the seed is updated.
+!
+!  N       (input) INTEGER
+!          The number of random numbers to be generated.
+!
+!  X       (output) Complex*16 array, dimension (N)
+!          The generated random numbers.
+!
+!\Author: Kristi Maschhoff
+!
+!\Details
+!
+!  Simple parallel version of LAPACK auxiliary routine zlarnv
+!  for X distributed across a 1-D array of processors.
+!  This routine calls the auxiliary routine CLARNV to generate random
+!  Complex*16 numbers from a uniform or normal distribution. Output is consistent
+!  with serial version.
+!
+!\SCCS Information:
+! FILE: larnv.F   SID: 1.3   DATE OF SID: 04/17/99
+!
+!-----------------------------------------------------------------------
+!
       subroutine pzlarnv( comm, idist, iseed, n, x )
-c
+!
       integer   comm
-c     ..
-c     .. Scalar Arguments ..
+!     ..
+!     .. Scalar Arguments ..
       integer			idist, n
-c     ..
-c     .. Array Arguments ..
+!     ..
+!     .. Array Arguments ..
       integer			iseed( 4 )
       Complex*16
      &                          x( * )
-c     ..
-c     .. External Subroutines ..
+!     ..
+!     .. External Subroutines ..
       external			zlarnv
-c     ..
-c     .. Executable Statements ..
-c
+!     ..
+!     .. Executable Statements ..
+!
       call zlarnv ( idist, iseed, n, x )
-c
+!
       return
       end
