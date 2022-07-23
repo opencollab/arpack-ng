568f4ea788d2db12a16a63e376b09b998d6df244
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:13 +0200

    ./PARPACK/SRC/MPI/pdneupd.F90: use debugF90 header.

--- PARPACK/SRC/MPI/pdneupd.F90
+++ PARPACK/SRC/MPI/pdneupd.F90
@@ -324,7 +324,7 @@
 !     | Include files for debugging and timing information |
 !     %----------------------------------------------------%
 !
-      include   'debug.h'
+#include "debugF90.h"
       include   'stat.h'
 !
 !     %------------------%
