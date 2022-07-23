8b2e60abf913fdaad0d67d6992c7c9d268d16ae4
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:00 +0200

    ./PARPACK/SRC/MPI/pcnaupd.F90: add & at end of continued line.

--- PARPACK/SRC/MPI/pcnaupd.F90
+++ PARPACK/SRC/MPI/pcnaupd.F90
@@ -385,8 +385,8 @@
 !
 !-----------------------------------------------------------------------
 !
-      subroutine pcnaupd
-     &   ( comm, ido, bmat, n, which, nev, tol, resid, ncv, v, ldv,
+      subroutine pcnaupd&
+     &   ( comm, ido, bmat, n, which, nev, tol, resid, ncv, v, ldv,&
      &     iparam, ipntr, workd, workl, lworkl, rwork, info )
 !
 #include "pcontextF90.h"
@@ -416,7 +416,7 @@
 !
       character  bmat*1, which*2
       integer    ido, info, ldv, lworkl, n, ncv, nev
-      Real
+      Real&
      &           tol
 !
 !     %-----------------%
@@ -424,16 +424,16 @@
 !     %-----------------%
 !
       integer    iparam(11), ipntr(14)
-      Complex
+      Complex&
      &           resid(n), v(ldv,ncv), workd(3*n), workl(lworkl)
-      Real
+      Real&
      &           rwork(ncv)
 !
 !     %------------%
 !     | Parameters |
 !     %------------%
 !
-      Complex
+      Complex&
      &           one, zero
       parameter (one = (1.0, 0.0) , zero = (0.0, 0.0) )
 !
@@ -441,11 +441,11 @@
 !     | Local Scalars |
 !     %---------------%
 !
-      integer    bounds, ierr, ih, iq, ishift, iupd, iw,
-     &           ldh, ldq, levec, mode, msglvl, mxiter, nb,
+      integer    bounds, ierr, ih, iq, ishift, iupd, iw,&
+     &           ldh, ldq, levec, mode, msglvl, mxiter, nb,&
      &           nev0, next, np, ritz, j
-      save       bounds, ih, iq, ishift, iupd, iw,
-     &           ldh, ldq, levec, mode, msglvl, mxiter, nb,
+      save       bounds, ih, iq, ishift, iupd, iw,&
+     &           ldh, ldq, levec, mode, msglvl, mxiter, nb,&
      &           nev0, next, np, ritz
 !
 !     %----------------------%
@@ -458,7 +458,7 @@
 !     | External Functions |
 !     %--------------------%
 !
-      Real
+      Real&
      &           pslamch10
       external   pslamch10
 !
@@ -510,11 +510,11 @@
              ierr = -3
          else if (mxiter .le. 0) then
              ierr = -4
-         else if (which .ne. 'LM' .and.
-     &       which .ne. 'SM' .and.
-     &       which .ne. 'LR' .and.
-     &       which .ne. 'SR' .and.
-     &       which .ne. 'LI' .and.
+         else if (which .ne. 'LM' .and.&
+     &       which .ne. 'SM' .and.&
+     &       which .ne. 'LR' .and.&
+     &       which .ne. 'SR' .and.&
+     &       which .ne. 'LI' .and.&
      &       which .ne. 'SI') then
             ierr = -5
          else if (bmat .ne. 'I' .and. bmat .ne. 'G') then
@@ -543,8 +543,8 @@
 !
          if (nb .le. 0)	nb = 1
          if (tol .le. 0.0  ) tol = pslamch10(comm, 'EpsMach')
-         if (ishift .ne. 0  .and.
-     &       ishift .ne. 1  .and.
+         if (ishift .ne. 0  .and.&
+     &       ishift .ne. 1  .and.&
      &       ishift .ne. 2)	ishift = 1
 !
 !        %----------------------------------------------%
@@ -602,10 +602,10 @@
 !     | Carry out the Implicitly restarted Arnoldi Iteration. |
 !     %-------------------------------------------------------%
 !
-      call pcnaup2
-     &   ( comm, ido, bmat, n, which, nev0, np, tol, resid, mode, iupd,
-     &     ishift, mxiter, v, ldv, workl(ih), ldh, workl(ritz),
-     &     workl(bounds), workl(iq), ldq, workl(iw),
+      call pcnaup2&
+     &   ( comm, ido, bmat, n, which, nev0, np, tol, resid, mode, iupd,&
+     &     ishift, mxiter, v, ldv, workl(ih), ldh, workl(ritz),&
+     &     workl(bounds), workl(iq), ldq, workl(iw),&
      &     ipntr, workd, rwork, info )
 !
 !     %--------------------------------------------------%
@@ -631,13 +631,13 @@
       if (info .eq. 2) info = 3
 !
       if (msglvl .gt. 0) then
-         call pivout (comm, logfil, 1, [mxiter], ndigit,
+         call pivout (comm, logfil, 1, [mxiter], ndigit,&
      &               '_naupd: Number of update iterations taken')
-         call pivout (comm, logfil, 1, [np], ndigit,
+         call pivout (comm, logfil, 1, [np], ndigit,&
      &               '_naupd: Number of wanted "converged" Ritz values')
-         call pcvout (comm, logfil, np, workl(ritz), ndigit,
+         call pcvout (comm, logfil, np, workl(ritz), ndigit,&
      &               '_naupd: The final Ritz values')
-         call pcvout (comm, logfil, np, workl(bounds), ndigit,
+         call pcvout (comm, logfil, np, workl(bounds), ndigit,&
      &               '_naupd: Associated Ritz estimates')
       end if
 !
@@ -653,35 +653,35 @@
 !        %--------------------------------------------------------%
 !
          write (6,1000)
-         write (6,1100) mxiter, nopx, nbx, nrorth, nitref, nrstrt,
-     &                  tmvopx, tmvbx, tcaupd, tcaup2, tcaitr, titref,
+         write (6,1100) mxiter, nopx, nbx, nrorth, nitref, nrstrt,&
+     &                  tmvopx, tmvbx, tcaupd, tcaup2, tcaitr, titref,&
      &                  tgetv0, tceigh, tcgets, tcapps, tcconv, trvec
- 1000    format (//,
-     &      5x, '=============================================',/
-     &      5x, '= Complex implicit Arnoldi update code      =',/
-     &      5x, '= Version Number: ', ' 2.1' , 21x, ' =',/
-     &      5x, '= Version Date:   ', ' 3/19/97' , 16x,   ' =',/
-     &      5x, '=============================================',/
-     &      5x, '= Summary of timing statistics              =',/
+ 1000    format (//,&
+     &      5x, '=============================================',/&
+     &      5x, '= Complex implicit Arnoldi update code      =',/&
+     &      5x, '= Version Number: ', ' 2.1' , 21x, ' =',/&
+     &      5x, '= Version Date:   ', ' 3/19/97' , 16x,   ' =',/&
+     &      5x, '=============================================',/&
+     &      5x, '= Summary of timing statistics              =',/&
      &      5x, '=============================================',//)
- 1100    format (
-     &      5x, 'Total number update iterations             = ', i5,/
-     &      5x, 'Total number of OP*x operations            = ', i5,/
-     &      5x, 'Total number of B*x operations             = ', i5,/
-     &      5x, 'Total number of reorthogonalization steps  = ', i5,/
-     &      5x, 'Total number of iterative refinement steps = ', i5,/
-     &      5x, 'Total number of restart steps              = ', i5,/
-     &      5x, 'Total time in user OP*x operation          = ', f12.6,/
-     &      5x, 'Total time in user B*x operation           = ', f12.6,/
-     &      5x, 'Total time in Arnoldi update routine       = ', f12.6,/
-     &      5x, 'Total time in p_naup2 routine              = ', f12.6,/
-     &      5x, 'Total time in basic Arnoldi iteration loop = ', f12.6,/
-     &      5x, 'Total time in reorthogonalization phase    = ', f12.6,/
-     &      5x, 'Total time in (re)start vector generation  = ', f12.6,/
-     &      5x, 'Total time in Hessenberg eig. subproblem   = ', f12.6,/
-     &      5x, 'Total time in getting the shifts           = ', f12.6,/
-     &      5x, 'Total time in applying the shifts          = ', f12.6,/
-     &      5x, 'Total time in convergence testing          = ', f12.6,/
+ 1100    format (&
+     &      5x, 'Total number update iterations             = ', i5,/&
+     &      5x, 'Total number of OP*x operations            = ', i5,/&
+     &      5x, 'Total number of B*x operations             = ', i5,/&
+     &      5x, 'Total number of reorthogonalization steps  = ', i5,/&
+     &      5x, 'Total number of iterative refinement steps = ', i5,/&
+     &      5x, 'Total number of restart steps              = ', i5,/&
+     &      5x, 'Total time in user OP*x operation          = ', f12.6,/&
+     &      5x, 'Total time in user B*x operation           = ', f12.6,/&
+     &      5x, 'Total time in Arnoldi update routine       = ', f12.6,/&
+     &      5x, 'Total time in p_naup2 routine              = ', f12.6,/&
+     &      5x, 'Total time in basic Arnoldi iteration loop = ', f12.6,/&
+     &      5x, 'Total time in reorthogonalization phase    = ', f12.6,/&
+     &      5x, 'Total time in (re)start vector generation  = ', f12.6,/&
+     &      5x, 'Total time in Hessenberg eig. subproblem   = ', f12.6,/&
+     &      5x, 'Total time in getting the shifts           = ', f12.6,/&
+     &      5x, 'Total time in applying the shifts          = ', f12.6,/&
+     &      5x, 'Total time in convergence testing          = ', f12.6,/&
      &      5x, 'Total time in computing final Ritz vectors = ', f12.6/)
          end if
       end if
