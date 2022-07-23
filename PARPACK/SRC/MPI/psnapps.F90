ffa790d4bbfd5b76157298cdd7e0b140e25eaba2
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:32 +0200

    ./PARPACK/SRC/MPI/psnapps.F90: use statF90 header.

--- PARPACK/SRC/MPI/psnapps.F90
+++ PARPACK/SRC/MPI/psnapps.F90
@@ -159,7 +159,7 @@
 !     %----------------------------------------------------%
 !
 #include "debugF90.h"
-      include   'stat.h'
+#include "statF90.h"
 !
 !     %------------------%
 !     | Scalar Arguments |
