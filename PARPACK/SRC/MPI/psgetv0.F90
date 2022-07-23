98e05bbd26b4bb5ea679d24abfd7792da469a740
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:28 +0200

    ./PARPACK/SRC/MPI/psgetv0.F90: use debugF90 header.

--- PARPACK/SRC/MPI/psgetv0.F90
+++ PARPACK/SRC/MPI/psgetv0.F90
@@ -149,7 +149,7 @@
 !     | Include files for debugging and timing information |
 !     %----------------------------------------------------%
 !
-      include   'debug.h'
+#include "debugF90.h"
       include   'stat.h'
 !
 !     %------------------%
