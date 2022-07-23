e65c9316b84bfac6d1fbfe329e4f5507f159e40d
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:38:00 +0200

    ./PARPACK/SRC/MPI/pzngets.F90: add & at end of continued line.

--- PARPACK/SRC/MPI/pzngets.F90
+++ PARPACK/SRC/MPI/pzngets.F90
@@ -121,14 +121,14 @@
 !     | Array Arguments |
 !     %-----------------%
 !
-      Complex*16
+      Complex*16&
      &           bounds(kev+np), ritz(kev+np)
 !
 !     %------------%
 !     | Parameters |
 !     %------------%
 !
-      Complex*16
+      Complex*16&
      &           one, zero
       parameter (one = (1.0, 0.0), zero = (0.0, 0.0))
 !
@@ -179,9 +179,9 @@
       if (msglvl .gt. 0) then
          call pivout (comm, logfil, 1, [kev], ndigit, '_ngets: KEV is')
          call pivout (comm, logfil, 1, [np], ndigit, '_ngets: NP is')
-         call pzvout (comm, logfil, kev+np, ritz, ndigit,
+         call pzvout (comm, logfil, kev+np, ritz, ndigit,&
      &        '_ngets: Eigenvalues of current H matrix ')
-         call pzvout (comm, logfil, kev+np, bounds, ndigit,
+         call pzvout (comm, logfil, kev+np, bounds, ndigit,&
      &      '_ngets: Ritz estimates of the current KEV+NP Ritz values')
       end if
 !
