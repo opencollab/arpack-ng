6c34dc54655ce8c88224be67fcf140c09eedbf2d
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:38 +0200

    ./PARPACK/SRC/MPI/psngets.F90: add & at end of continued line.

--- PARPACK/SRC/MPI/psngets.F90
+++ PARPACK/SRC/MPI/psngets.F90
@@ -99,8 +99,8 @@
 !
 !-----------------------------------------------------------------------
 !
-      subroutine psngets
-     &                 ( comm, ishift, which, kev, np, ritzr, ritzi,
+      subroutine psngets&
+     &                 ( comm, ishift, which, kev, np, ritzr, ritzi,&
      &                   bounds, shiftr, shifti )
 !
 !     %--------------------%
@@ -127,15 +127,15 @@
 !     | Array Arguments |
 !     %-----------------%
 !
-      Real
-     &           bounds(kev+np), ritzr(kev+np), ritzi(kev+np),
+      Real&
+     &           bounds(kev+np), ritzr(kev+np), ritzi(kev+np),&
      &           shiftr(1), shifti(1)
 !
 !     %------------%
 !     | Parameters |
 !     %------------%
 !
-      Real
+      Real&
      &           one, zero
       parameter (one = 1.0, zero = 0.0)
 !
@@ -202,7 +202,7 @@
 !     | complex conjugate pairs together.                     |
 !     %-------------------------------------------------------%
 !
-      if (       ( ritzr(np+1) - ritzr(np) ) .eq. zero
+      if (       ( ritzr(np+1) - ritzr(np) ) .eq. zero&
      &     .and. ( ritzi(np+1) + ritzi(np) ) .eq. zero ) then
          np = np - 1
          kev = kev + 1
@@ -228,11 +228,11 @@
       if (msglvl .gt. 0) then
          call pivout (comm, logfil, 1, [kev], ndigit, '_ngets: KEV is')
          call pivout (comm, logfil, 1, [np], ndigit, '_ngets: NP is')
-         call psvout (comm, logfil, kev+np, ritzr, ndigit,
+         call psvout (comm, logfil, kev+np, ritzr, ndigit,&
      &        '_ngets: Eigenvalues of current H matrix -- real part')
-         call psvout (comm, logfil, kev+np, ritzi, ndigit,
+         call psvout (comm, logfil, kev+np, ritzi, ndigit,&
      &        '_ngets: Eigenvalues of current H matrix -- imag part')
-         call psvout (comm, logfil, kev+np, bounds, ndigit,
+         call psvout (comm, logfil, kev+np, bounds, ndigit,&
      &      '_ngets: Ritz estimates of the current KEV+NP Ritz values')
       end if
 !
