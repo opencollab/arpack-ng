90183288ced2588844ddc394f6c1710fbff315b5
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:24 +0200

    ./PARPACK/SRC/MPI/pdsgets.F90: use debugF90 header.

--- PARPACK/SRC/MPI/pdsgets.F90
+++ PARPACK/SRC/MPI/pdsgets.F90
@@ -110,7 +110,7 @@
 !     | Include files for debugging and timing information |
 !     %----------------------------------------------------%
 !
-      include   'debug.h'
+#include "debugF90.h"
       include   'stat.h'
 !
 !     %------------------%
