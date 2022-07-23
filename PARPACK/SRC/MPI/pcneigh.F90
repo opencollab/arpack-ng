0d19856531ce87de7e5ce47a7b927261dc1c3d9d
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-07-23 10:37:01 +0200

    ./PARPACK/SRC/MPI/pcneigh.F90: use debugF90 header.

--- PARPACK/SRC/MPI/pcneigh.F90
+++ PARPACK/SRC/MPI/pcneigh.F90
@@ -120,7 +120,7 @@
 !     | Include files for debugging and timing information |
 !     %----------------------------------------------------%
 !
-      include   'debug.h'
+#include "debugF90.h"
       include   'stat.h'
 !
 !     %------------------%
