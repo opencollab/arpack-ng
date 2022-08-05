8c79b624c4da3b940e89da84abd9b07f37f55b13
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-08-05 21:46:49 +0200

    ./PARPACK/UTIL/MPI/pdvout.F90: comment are !, not *.

--- PARPACK/UTIL/MPI/pdvout.F90
+++ PARPACK/UTIL/MPI/pdvout.F90
@@ -1,83 +1,83 @@
-*  Routine:    PDVOUT - Parallel Version of ARPACK utility routine DVOUT
-*
-*  Purpose:    Double precision vector output routine.
-*
-*  Usage:      CALL PDVOUT (COMM, LOUT, N, SX, IDIGIT, IFMT)
-*
-*  Arguments
-*     COMM   - MPI Communicator for the processor grid
-*     N      - Length of array SX.  (Input)
-*     SX     - Double precision array to be printed.  (Input)
-*     IFMT   - Format to be used in printing array SX.  (Input)
-*     IDIGIT - Print up to IABS(IDIGIT) decimal digits per number.  (In)
-*              If IDIGIT .LT. 0, printing is done with 72 columns.
-*              If IDIGIT .GT. 0, printing is done with 132 columns.
-*
-*\SCCS Information:
-* FILE: vout.F   SID: 1.2   DATE OF SID: 3/19/97   RELEASE: 1
-*
-*-----------------------------------------------------------------------
-*
+!  Routine:    PDVOUT - Parallel Version of ARPACK utility routine DVOUT
+!
+!  Purpose:    Double precision vector output routine.
+!
+!  Usage:      CALL PDVOUT (COMM, LOUT, N, SX, IDIGIT, IFMT)
+!
+!  Arguments
+!     COMM   - MPI Communicator for the processor grid
+!     N      - Length of array SX.  (Input)
+!     SX     - Double precision array to be printed.  (Input)
+!     IFMT   - Format to be used in printing array SX.  (Input)
+!     IDIGIT - Print up to IABS(IDIGIT) decimal digits per number.  (In)
+!              If IDIGIT .LT. 0, printing is done with 72 columns.
+!              If IDIGIT .GT. 0, printing is done with 132 columns.
+!
+!\SCCS Information:
+! FILE: vout.F   SID: 1.2   DATE OF SID: 3/19/97   RELEASE: 1
+!
+!-----------------------------------------------------------------------
+!
       SUBROUTINE PDVOUT( COMM , LOUT, N, SX, IDIGIT, IFMT )
-*     ...
+!     ...
       include  'mpif.h'
-*
-*     .. MPI VARIABLES AND FUNCTIONS ..
-*     .. Variable Declaration ..
+!
+!     .. MPI VARIABLES AND FUNCTIONS ..
+!     .. Variable Declaration ..
       integer    COMM, MYID, IERR
-*
-*     ... SPECIFICATIONS FOR ARGUMENTS
-*     ...
-*     ... SPECIFICATIONS FOR LOCAL VARIABLES
-*     .. Scalar Arguments ..
+!
+!     ... SPECIFICATIONS FOR ARGUMENTS
+!     ...
+!     ... SPECIFICATIONS FOR LOCAL VARIABLES
+!     .. Scalar Arguments ..
       CHARACTER*( * )    IFMT
       INTEGER            IDIGIT, LOUT, N
-*     ..
-*     .. Array Arguments ..
+!     ..
+!     .. Array Arguments ..
       Double precision
      &                   SX( * )
-*     ..
-*     .. Local Scalars ..
+!     ..
+!     .. Local Scalars ..
       CHARACTER*80       LINE
       INTEGER            I, K1, K2, LLL, NDIGIT
-*     ..
-*     .. Intrinsic Functions ..
+!     ..
+!     .. Intrinsic Functions ..
       INTRINSIC          LEN, MIN, MIN0
-*     ..
-*     .. Executable Statements ..
-*     ...
-*     ... FIRST EXECUTABLE STATEMENT
-*
-*     Determine processor configuration
-*
+!     ..
+!     .. Executable Statements ..
+!     ...
+!     ... FIRST EXECUTABLE STATEMENT
+!
+!     Determine processor configuration
+!
       call MPI_COMM_RANK( comm, myid, ierr )
-*
-*     .. Only Processor 0 will write to file LOUT ..
-*
+!
+!     .. Only Processor 0 will write to file LOUT ..
+!
       IF ( MYID .EQ. 0 ) THEN
-*
+!
       LLL = MIN( LEN( IFMT ), 80 )
       DO 10 I = 1, LLL
          LINE( I: I ) = '-'
    10 CONTINUE
-*
+!
       DO 20 I = LLL + 1, 80
          LINE( I: I ) = ' '
    20 CONTINUE
-*
+!
       WRITE( LOUT, FMT = 9999 )IFMT, LINE( 1: LLL )
  9999 FORMAT( / 1X, A, / 1X, A )
-*
+!
       IF( N.LE.0 )
      $   RETURN
       NDIGIT = IDIGIT
       IF( IDIGIT.EQ.0 )
      $   NDIGIT = 4
-*
-*=======================================================================
-*             CODE FOR OUTPUT USING 72 COLUMNS FORMAT
-*=======================================================================
-*
+!
+!=======================================================================
+!             CODE FOR OUTPUT USING 72 COLUMNS FORMAT
+!=======================================================================
+!
       IF( IDIGIT.LT.0 ) THEN
          NDIGIT = -IDIGIT
          IF( NDIGIT.LE.4 ) THEN
@@ -101,11 +101,11 @@
                WRITE( LOUT, FMT = 9995 )K1, K2, ( SX( I ), I = K1, K2 )
    60       CONTINUE
          END IF
-*
-*=======================================================================
-*             CODE FOR OUTPUT USING 132 COLUMNS FORMAT
-*=======================================================================
-*
+!
+!=======================================================================
+!             CODE FOR OUTPUT USING 132 COLUMNS FORMAT
+!=======================================================================
+!
       ELSE
          IF( NDIGIT.LE.4 ) THEN
             DO 70 K1 = 1, N, 10
