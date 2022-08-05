b1e03c6702cae73aa16d3f1f127aa66152a24b74
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-08-05 21:46:52 +0200

    ./PARPACK/UTIL/MPI/psvout.F90: use ICB module provided by MPI.

--- PARPACK/UTIL/MPI/psvout.F90
+++ PARPACK/UTIL/MPI/psvout.F90
@@ -20,7 +20,12 @@
 !
       SUBROUTINE PSVOUT( COMM , LOUT, N, SX, IDIGIT, IFMT )
 !     ...
-      include  'mpif.h'
+#ifdef HAVE_MPI_ICB
+      use :: mpi_f08
+#else
+#include "mpif.h"
+#endif
+
 !
 !     .. MPI VARIABLES AND FUNCTIONS ..
 !     .. Variable Declaration ..
